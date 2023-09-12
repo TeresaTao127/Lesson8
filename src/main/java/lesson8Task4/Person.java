2307efc8542a4b9fadd03934a56036e5c46743b0
TeresaTao127 <taoleiluck2020@gmail.com>
2023-09-12 14:40:09 +0100

    lesson8 Task4

---
+++ src/main/java/lesson8Task4/Person.java
@@ -0,0 +1,52 @@
+package lesson8Task4;
+
+import java.util.List;
+
+public class Person {
+    String name;
+    String lastName;
+    int yearOfBirth;
+    double height;
+    double weight;
+
+    public Person(String name, String lastName, int yearOfBirth, double height, double weight) {
+        this.name = name;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                ", height=" + height +
+                ", weight=" + weight +
+                '}';
+    }
+
+
+}
